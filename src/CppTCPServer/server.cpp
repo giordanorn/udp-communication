f348b7b9633b33db8decd17f3a4c319e7417205d
josenaldo <josenaldo.asj@gmail.com>
2019-06-18 14:16:46 -0300

    Added possible changes

--- src/CppTCPServer/server.cpp
+++ src/CppTCPServer/server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <unordered_map>
+#include <unordered_set>
 #include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -13,7 +14,7 @@ using namespace std;
 #define CLIENT_MSG_SIZE 2000
 
 struct Client{
-    int port;
+    u_short port;
     string addr;
 
     bool operator==(const Client &other) const {return port == other.port && addr == other.addr;}
@@ -24,11 +25,11 @@ namespace std {
     template <>
     struct hash<Client>
     {
-        size_t operator()(const Client& client) const {return hash<string>()(client.addr) ^ (hash<int>()(client.port) << 1);}
+        size_t operator()(const Client& client) const {return hash<string>()(client.addr) ^ (hash<u_short>()(client.port) << 1);}
     };
 }
 
-void listenToClient(int sock){
+void listenToClient(int sock, Client client){
     char clientMessage[CLIENT_MSG_SIZE];
 
     while(true){
@@ -38,10 +39,40 @@ void listenToClient(int sock){
     }
 }
 
-unordered_map<Client, thread> clientThreads;
+void waitsForANewConnection(int serverSocket, unordered_map<Client, thread>& clientThreads){
+    // Address of the incoming client
+    struct sockaddr_in clientAddress;
+    unsigned int sizeClientAddress = sizeof clientAddress;
+
+    // Waits for a new connection
+    cout << "Waiting for a new connection...\n";
+    int socketToCommunicateWithClient = accept(serverSocket, (struct sockaddr*) &clientAddress, &sizeClientAddress);
+
+    // Gets the client's address in IPv4 notation
+    char ipAddressAsCharArray[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(clientAddress.sin_addr), ipAddressAsCharArray, INET_ADDRSTRLEN);
+    string address = string(ipAddressAsCharArray);
+
+    // Gets the client's port
+    u_short port = clientAddress.sin_port;
+
+    // Creates a new Client object. 
+    Client client = {port, address};
+
+    // Creates a thread for the new client
+    thread threadClient;
+    threadClient = thread(listenToClient, socketToCommunicateWithClient, client);
+
+    // Adds the thread in the map of threads
+    clientThreads[client] = move(threadClient);
+}
+
 
 // Adapted from: https://dzone.com/articles/parallel-tcpip-socket-server-with-multi-threading
 int main(int argc, char** argv){
+    unordered_map<Client, thread> clientThreads;
+    unordered_set<string> usedNames;
+
     int port = atoi(argv[1]);
 
     // Result code from the socket's functions
@@ -62,50 +93,25 @@ int main(int argc, char** argv){
         cout << "Binding done.\n";
     }
 
-    else{
+    else {
         cout << "ERROR: can't binding on the socket. "  << strerror(resultCode) << endl;   
         exit(0);
     }
 
     // Makes the server listen on the socket
-    if((resultCode = listen(serverSocket,MAX_CLIENTS))==0){
+    if ((resultCode = listen(serverSocket, MAX_CLIENTS))==0){
         cout << "Listening on the socket." << endl;
     }
 
-    else{
+    else {
         cout << "ERROR: can't listen on the socket. "  << strerror(resultCode) << endl;
         exit(0);
     }
 
-
-    thread threadClient;
-
-    while(true){
-
-        // Waits for a new connection
-        cout << "Waiting for a new connection...\n";
-        struct sockaddr_in peerAddress;
-        unsigned int sizePeerAddress = sizeof peerAddress;
-        int socketToCommunicateWithClient = accept(serverSocket, (struct sockaddr*) &peerAddress, &sizePeerAddress);
-
-        // Creates a new Client object.
-        // TODO: put here the *actual* client's address and port.
-        string a("o"); 
-        Client client = Client();
-        client.port = 1;
-        client.addr = a;
-
-        // Creates a thread for the new client
-        threadClient = thread(listenToClient, socketToCommunicateWithClient);
-
-        // Adds the thread in the map of threads
-        clientThreads[client] = move(threadClient);
-
+    while (true)
+    {
+        waitsForANewConnection(serverSocket, clientThreads);
     }
-
-
     
-
-
     return 0;
 }
